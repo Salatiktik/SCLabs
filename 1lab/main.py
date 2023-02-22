d0c08ceafbc2d25872eac9e2fbdec7f164c4487c
Salatiktik <muz.ugol@gmail.com>
2023-02-22 21:04:41 +0300

    2 task add

--- 1lab/main.py
+++ 1lab/main.py
@@ -1,1 +1,25 @@
+import constants as cnst
+
+def operation(first:float or int,second:float or int,operator:str):
+    if(operator==cnst.OPERATIONS[0]):
+        return first+second
+    elif(operator==cnst.OPERATIONS[1]):
+        return first-second
+    elif(operator==cnst.OPERATIONS[2]):
+        return first*second
+    elif(operator==cnst.OPERATIONS[3]):
+        return first/second
+    else:
+        return "error: wrong operation string, try add/sub/mult/div"
+
+print("Part 1:")
 print("Hello world")
+
+print("Part 2:")
+print("\nenter first, second numbers and operation add/sub/mult/div:")
+first = int(input(int))
+second = int(input(int))
+operator = input(str)
+
+print("The result of operation: ", operation(first,second,operator))
+
